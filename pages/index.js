41383d7959cc6b82dc73a1163ade580ef21f984d
harymunnli <prgmrharry@gmail.com>
2023-01-13 16:33:53 +0700

    ready for client

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from '@next/font/google'
 import YouTubeIcon  from '@material-ui/icons/YouTube'
 import FacebookIcon from '@material-ui/icons/Facebook'
 import InstagramIcon from '@material-ui/icons/Instagram'
+import { Face } from '@material-ui/icons'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,8 +24,8 @@ export default function Home() {
         <div className="flex float-left mx-8">
             <img src="assets/logo_sekolah.png" alt="Logo Sekolah" width="40px" height="40px" className=""/>
             <span id="nama-sekolah" className="grid-rows-2 text-sm mx-4 align-middle">
-                <p className="text-start">SMP MUHAMMADIYAH 1</p>
-                <p className="text-start">DENPASAR</p>
+                <p className="text-start">SMAN 2</p>
+                <p className="text-start">Sawahlunto</p>
             </span>
         </div>
 
@@ -69,7 +70,8 @@ export default function Home() {
         <div class="mx-32 space-y-4">
             <div class="relative border-4 shadow-lg">
                 <div class="inline-block top-0 py-2 px-4">Alur Pendaftaran</div>
-                <button class="w-6 h-6 absolute right-2 top-0"><svg class="w-6 h-6 absolute right-0 top-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                <button id="alpen-btn"class="w-6 h-6 absolute right-2 top-0"><svg class="w-6 h-6 absolute right-0 top-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                <p id="alpen" className='px-4 hidden'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur semper mauris id est ullamcorper consectetur. Morbi pellentesque aliquam libero, ut porta odio ullamcorper eget. Morbi molestie nisi augue, nec lacinia lacus maximus quis. Sed leo ipsum, cursus a vestibulum ut, rhoncus nec quam. Nunc commodo mi tempor nunc malesuada, ut vulputate metus eleifend. Donec elit diam, rutrum tempus convallis vel, pharetra quis elit. Morbi ex eros, molestie id ullamcorper in, egestas ac mi. Quisque id tortor ut felis vulputate efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis at tincidunt ex. Phasellus nec nisl tincidunt, pellentesque lacus ac, interdum lectus. Nulla vitae turpis et lorem suscipit hendrerit posuere a diam. Praesent volutpat mi sapien, porttitor sodales nisl aliquet nec.</p>
             </div>
             <div class="relative border-4 shadow-lg">
                 <div class="inline-block top-0 py-2 px-4">Syarat Pendaftaran</div>
@@ -77,18 +79,19 @@ export default function Home() {
                 <ol id="syapen" class="list-decimal ml-8 hidden">
                   <li class="text-sm">Beragama Islam</li>
                   <li class="text-sm">Memiliki NISN</li>
-                  <li class="text-sm">Berusaha maksimum 15 tahun pada 1 Juli 2023</li>
+                  <li class="text-sm">Berusia maksimum 15 tahun pada 1 Juli 2023</li>
                   <li class="text-sm">Berkas yang harus dilengkapi pada waktu Pendaftaran:</li>
                   <ul class="list-disc ml-4">
                     <li class="text-sm">a. Mengisi Formulir Pendaftaran</li>
                     <li class="text-sm">b. Uang pendaftaran Rp.300.000</li>
-                    <li class="text-sm">c. Pas foto warna 3x4 background merah sejumlah 4 lembar untuk putri berjilbab putuh</li>
+                    <li class="text-sm">c. Pas foto warna 3x4 background merah sejumlah 4 lembar</li>
                   </ul>
                 </ol>
             </div>
             <div class="relative border-4 shadow-lg">
                 <div class="inline-block top-0 py-2 px-4">Informasi Tes PPDB</div>
-                <button class="w-6 h-6 absolute right-2 top-0"><svg class="w-6 h-6 absolute right-0 top-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                <button id="intes_btn" class="intes-btn w-6 h-6 absolute right-2 top-0"><svg class="w-6 h-6 absolute right-0 top-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                <p id="intes" className='px-4 hidden'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur semper mauris id est ullamcorper consectetur. Morbi pellentesque aliquam libero, ut porta odio ullamcorper eget. Morbi molestie nisi augue, nec lacinia lacus maximus quis. Sed leo ipsum, cursus a vestibulum ut, rhoncus nec quam. Nunc commodo mi tempor nunc malesuada, ut vulputate metus eleifend. Donec elit diam, rutrum tempus convallis vel, pharetra quis elit. Morbi ex eros, molestie id ullamcorper in, egestas ac mi. Quisque id tortor ut felis vulputate efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis at tincidunt ex. Phasellus nec nisl tincidunt, pellentesque lacus ac, interdum lectus. Nulla vitae turpis et lorem suscipit hendrerit posuere a diam. Praesent volutpat mi sapien, porttitor sodales nisl aliquet nec.</p>
             </div> 
         </div>       
       </div>
@@ -97,21 +100,21 @@ export default function Home() {
       <p className='text-4xl text-center border-b-4 border-yellow-400 mx-[100px] mb-4 mt-10'> Kontak Kami </p>
       <section className="grid grid-cols-2 bg-indigo-500 p-4 mx-8 rounded-lg">
         <div className=" flex-row">
-          <p className="text-center text-col text-white mt-8 xl mb-4"> Hubungi Kami </p>
           <div className='ml-6'>
             <form className="flex flex-col">
               <input className='rounded-full mt-4 mb-6' placeholder="Nama Anda..."></input>
-              <textarea className='rounded-lg mb-12' placeholder="Pesan Anda..." height="400px"></textarea>
+              <textarea className='rounded-lg mb-8' placeholder="Pesan Anda..." height="400px"></textarea>
             </form>
-            <div className='mb-2'><YouTubeIcon/> <span className='ml-2 mb-4 text-white'> Muhammadiyah 1 Denpasar </span></div>
-            <div className='mb-2'> <FacebookIcon/> <span className='ml-2 mb-4 text-white'>Muhammadiyah 1 Denpasar</span> </div>
+            <div className='mb-2'><YouTubeIcon/> <span className='ml-2 mb-4 text-white'> SMAN 2 Sawahlunto </span></div>
+            <div className='mb-2'> <FacebookIcon/> <span className='ml-2 mb-4 text-white'>SMAN 2 Sawahlunto</span> </div>
             <InstagramIcon/> <span className='ml-2 mb-4 text-white'> Mutu Denpasar </span>
           </div>
-            
         </div>   
         
-        <img src="assets/gmap_lokasi.png" alt='lokasi-google-map' className='rounded-lg' width="874px" height="688px"></img>
+        <img src="assets/gmap_lokasi.png" alt='lokasi-google-map' className='rounded-lg px-4 my-auto'></img>
       </section>
+
+      <div className='text-center bg-black text-white text-base'>From Figma Desain by IT Mutu Denpasar</div>
 
       <script src="index.js"></script>
       </main>
--- public/index.js
+++ public/index.js
@@ -1,7 +1,21 @@
 const syapen = document.getElementById("syapen")
 const btn   = document.getElementById("syapen-btn")
 
+const alpen = document.getElementById("alpen")
+const btn2 = document.getElementById("alpen-btn")
+
+const inftes = document.getElementById("intes")
+const inftes_btn = document.getElementById("intes-btn")
+
 
 btn.addEventListener("click", () => {
     syapen.classList.toggle("hidden");
+})
+
+btn2.addEventListener("click", () => {
+    alpen.classList.toggle("hidden");
+})
+
+intes_btn.addEventListener("click", () => {
+    inftes.classList.toggle("hidden");
 })
