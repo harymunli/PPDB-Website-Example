8e4520d66c8610b96e4fa64183630497e0a459f5
harymunnli <prgmrharry@gmail.com>
2023-01-20 12:45:10 +0700

    finishing register success page"

--- pages/success.js
+++ pages/success.js
@@ -26,19 +26,15 @@ export default class success extends React.Component{
                         <p className='text-center text-green-800 text-2xl'>Selamat Akun Berhasil Dibuat</p>
                         <p className='mt-2 text-center'>Untuk melanjutkan tahap berikutnya, silahkan melakukan</p>
                         <p className='mt-2 text-center'>Pembayaran biaya pendaftaran sebesar Rp 200.000</p>
-                        <div className="grid grid-cols-2 justify-items-stretch">
-                            <input type="text" value="93682739172" readonly className='col-start-1 border-2 shadow-lg mx-auto w-[150px]'></input>
-                            <span>
-                                <CopyToClipboard className="inline col-start-1" text="93682739172"
-                                    onCopy={() => this.setState({copied: true})}>
-                                    <button><FileCopyIcon/></button>
-                                </CopyToClipboard>
-                            </span>  
+                        <div className="flex flex-cols-2 space-x-1 mx-auto">
+                            <input type="text" value="93682739172" readonly className='text-center border-2 shadow-lg w-[150px]'/>
+                            <CopyToClipboard className="shrink" text="93682739172" 
+                                onCopy={() => this.setState({copied: true})}>
+                                <button><FileCopyIcon/></button>
+                            </CopyToClipboard>
                         </div>
-                            {this.state.copied ? <span style={{color: 'red'}}>Copied.</span> : null}
-                        <p className='mt-2 text-center'>a/n SMAN 0 Sawahlunto (Bank BRI)</p>
-    
-                        {/* TODO 2 Button. */}
+                            {this.state.copied ? <span className="text-center" style={{color: 'red'}}>Copied.</span> : null}
+                            <p className='mt-2 text-center'>a/n SMAN 0 Sawahlunto (Bank BRI)</p>
                     </div>
                 </main>
             </div>
